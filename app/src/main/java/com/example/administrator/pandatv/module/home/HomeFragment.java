cb43ea2d80a4597975f87d94e3d2bde4fefb4f6e
JackSir1 <15128150932@qq.com>
2017-07-14 09:02:45 +0800

    首页布局

--- app/src/main/java/com/example/administrator/pandatv/module/home/HomeFragment.java
+++ app/src/main/java/com/example/administrator/pandatv/module/home/HomeFragment.java
@@ -1,23 +1,33 @@
 package com.example.administrator.pandatv.module.home;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.administrator.pandatv.R;
 import com.example.administrator.pandatv.base.BaseFragment;
 import com.example.administrator.pandatv.model.entity.HomeBean;
+import com.example.administrator.pandatv.module.home.viewpager.HomeChinaLiveAdapter;
+import com.example.administrator.pandatv.module.home.viewpager.HomePandaLiveAdapter;
+import com.example.administrator.pandatv.module.home.viewpager.HomeSplendidAdapter;
 import com.example.administrator.pandatv.module.home.viewpager.HomeViewPagerAdapter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.Unbinder;
 
 /**
  * Created by Administrator on 2017/7/12.
@@ -50,8 +60,26 @@ public class HomeFragment extends BaseFragment implements HomeContract.View {
     TextView home;
     @BindView(R.id.home_liveChina_gridView)
     GridView homeLiveChinaGridView;
+    @BindView(R.id.home_viewpager)
+    ViewPager homeViewpager;
+    @BindView(R.id.home_viewpager_linearLayout)
+    LinearLayout homeViewpagerLinearLayout;
+    @BindView(R.id.home_pandaObserver_image)
+    ImageView homePandaObserverImage;
+    private int currmentNum = 100000;
+    private List<View> viewPagerFragments = new ArrayList<>();
+    private HomeContract.Presenter presenter;
+    private List<CheckBox> checkBoxes = new ArrayList<>();
 
-    private HomeViewPagerAdapter adapter;
+    private List<HomeBean.DataBean.PandaliveBean.ListBean> pandaliveBeans=new ArrayList<>();
+    private HomePandaLiveAdapter pandaLiveAdapter;
+
+    private List<HomeBean.DataBean.ChinaliveBean.ListBeanXX> chinaLiveBeans=new ArrayList<>();
+    private HomeChinaLiveAdapter chinaLiveAdapter;
+
+    private List<HomeBean.DataBean.AreaBean.ListscrollBean> listscrollBeens=new ArrayList<>();
+    private HomeSplendidAdapter splendidAdapter;
+
     @Override
     protected int getViweId() {
         return R.layout.home_fragment;
@@ -64,17 +92,39 @@ public class HomeFragment extends BaseFragment implements HomeContract.View {
 
     @Override
     protected void loadDate() {
-
+        presenter.start();
     }
 
     @Override
     protected void setListener() {
+        homeViewpager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
 
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                currmentNum = position;
+                for (int i = 0; i < checkBoxes.size(); i++) {
+                    if (i == currmentNum % checkBoxes.size()) {
+                        checkBoxes.get(i).setChecked(true);
+                    } else {
+                        checkBoxes.get(i).setChecked(false);
+                    }
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
     }
 
     @Override
     public void setPresenter(HomeContract.Presenter presenter) {
-
+        this.presenter = presenter;
 
     }
 
@@ -92,8 +142,110 @@ public class HomeFragment extends BaseFragment implements HomeContract.View {
     public void setResult(HomeBean homeBean) {
 
         List<HomeBean.DataBean.BigImgBean> bigImgBeanList = homeBean.getData().getBigImg();
+        HomeBean.DataBean.ChinaliveBean chinalive = homeBean.getData().getChinalive();
+        HomeBean.DataBean.PandaeyeBean pandaeye = homeBean.getData().getPandaeye();
+        HomeBean.DataBean.PandaliveBean pandalive = homeBean.getData().getPandalive();
+        HomeBean.DataBean.InteractiveBean interactive = homeBean.getData().getInteractive();
+        HomeBean.DataBean.AreaBean area = homeBean.getData().getArea();
+        showGG(area);
+        showLiveChina(chinalive);
+        showLivePanda(pandalive);
+        showPandaObserever(pandaeye);
+        showSplendid(area);
+        showViewPager(bigImgBeanList);
     }
-    public void showPandaObserever(){
+
+    //輪播圖
+    public void showViewPager(List<HomeBean.DataBean.BigImgBean> bigImgBeanList) {
+        View view = null;
+        CheckBox checkBox;
+        View view1 = null;
+        for (HomeBean.DataBean.BigImgBean bigImgBean : bigImgBeanList) {
+            view1 = LayoutInflater.from(getContext()).inflate(R.layout.checkbox_item, null);
+            checkBox = (CheckBox) view1.findViewById(R.id.viewpager_checkbox_btn);
+            homeViewpagerLinearLayout.addView(view1);
+            checkBoxes.add(checkBox);
+            view = LayoutInflater.from(getContext()).inflate(R.layout.home_viewpager_fragment, null);
+            ImageView imageView = (ImageView) view.findViewById(R.id.home_viewpager_image);
+            TextView title = (TextView) view.findViewById(R.id.home_viewpager_title);
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            String image = bigImgBean.getImage();
+            String titlestr = bigImgBean.getTitle();
+            Glide.with(getContext()).load(image).into(imageView);
+            title.setText(titlestr);
+            viewPagerFragments.add(view);
+        }
+        HomeViewPagerAdapter adapter = new HomeViewPagerAdapter(viewPagerFragments);
+        homeViewpager.setAdapter(adapter);
+        checkBoxes.get(currmentNum % checkBoxes.size()).setChecked(true);
+        homeViewpager.setCurrentItem(currmentNum);
+        handler.sendEmptyMessageDelayed(222, 2000);
+    }
+
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case 222:
+                    currmentNum++;
+                    homeViewpager.setCurrentItem(currmentNum);
+                    for (int i = 0; i < checkBoxes.size(); i++) {
+                        if (i == currmentNum % checkBoxes.size()) {
+                            checkBoxes.get(i).setChecked(true);
+                        } else {
+                            checkBoxes.get(i).setChecked(false);
+                        }
+                    }
+
+                    handler.sendEmptyMessageDelayed(222, 2000);
+                    break;
+            }
+        }
+    };
+
+    //熊貓播報
+    public void showPandaBoBao(){
+
+    }
+
+    //熊貓觀察
+    public void showPandaObserever(HomeBean.DataBean.PandaeyeBean pandaeye) {
+        String pandaeyelogo = pandaeye.getPandaeyelogo();
+        homePandaObserverImage.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        Glide.with(getContext()).load(pandaeyelogo).into(homePandaObserverImage);
+        homeObserverBtn1.setText(pandaeye.getItems().get(0).getBrief());
+        homeObserverBtn2.setText(pandaeye.getItems().get(1).getBrief());
+        homeObserverTitle1.setText(pandaeye.getItems().get(0).getTitle());
+        homeObserverTitle2.setText(pandaeye.getItems().get(0).getTitle());
+    }
+
+    //直播秀場
+    public void showLivePanda(HomeBean.DataBean.PandaliveBean pandalive) {
+        if (pandaliveBeans!=null)
+            pandaliveBeans.clear();
+        pandaliveBeans.addAll(pandalive.getList());
+        pandaLiveAdapter=new HomePandaLiveAdapter(getContext(),pandaliveBeans);
+        homePlayLive.setAdapter(pandaLiveAdapter);
+    }
+
+    //直播中國
+    public void showLiveChina(HomeBean.DataBean.ChinaliveBean chinalive) {
+        chinaLiveBeans.addAll(chinalive.getList());
+        chinaLiveAdapter=new HomeChinaLiveAdapter(getContext(),chinaLiveBeans);
+        homeLiveChinaGridView.setAdapter(chinaLiveAdapter);
+    }
+
+    //精彩一刻
+    public void showSplendid(HomeBean.DataBean.AreaBean area) {
+        listscrollBeens.addAll(area.getListscroll());
+        splendidAdapter=new HomeSplendidAdapter(getContext(),listscrollBeens);
+        homeSplendidGridView.setAdapter(splendidAdapter);
+
+    }
+
+    //滾滾視頻
+    public void showGG(HomeBean.DataBean.AreaBean area) {
 
     }
 
@@ -108,11 +260,5 @@ public class HomeFragment extends BaseFragment implements HomeContract.View {
         View rootView = super.onCreateView(inflater, container, savedInstanceState);
         unbinder = ButterKnife.bind(this, rootView);
         return rootView;
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        unbinder.unbind();
     }
 }
--- app/src/main/java/com/example/administrator/pandatv/module/home/viewpager/HomeViewPagerAdapter.java
+++ app/src/main/java/com/example/administrator/pandatv/module/home/viewpager/HomeViewPagerAdapter.java
@@ -1,38 +1,40 @@
 package com.example.administrator.pandatv.module.home.viewpager;
 
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.PagerAdapter;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.androidkun.adapter.BaseAdapter;
-import com.example.administrator.pandatv.base.BaseFragment;
-import com.example.administrator.pandatv.module.home.HomeFragment;
-
 import java.util.List;
-
 
 /**
  * Created by Administrator on 2017/7/13.
  */
 
-public class HomeViewPagerAdapter extends FragmentPagerAdapter {
-    private List<BaseFragment> fragments;
-
-    public HomeViewPagerAdapter(FragmentManager fm,List<BaseFragment> fragments) {
-        super(fm);
-        this.fragments=fragments;
+public class HomeViewPagerAdapter extends PagerAdapter {
+    private List<View> list;
+    public HomeViewPagerAdapter(List<View> list){
+        this.list=list;
     }
-
-    @Override
-    public Fragment getItem(int position) {
-        return fragments.get(position % fragments.size());
-    }
-
     @Override
     public int getCount() {
-        return fragments.size()<0?0:Integer.MAX_VALUE;
+        return list.size()>0?Integer.MAX_VALUE:0;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        if (list.size()>0)
+            container.removeView(list.get(position%list.size()));
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        if (list.size()>0)
+            container.addView(list.get(position%list.size()));
+        return list.get(position%list.size());
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view==object;
     }
 }
